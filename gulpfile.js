0830f2065d5b16b7b420d3177fae4811db7acb59
Charles Prakash Dasari <v-chdasa@microsoft.com>
2017-11-03 12:59:39 -0700

    Adding initial files

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var zip = require('gulp-zip');
+
+gulp.task('generate-manifest', function() {
+    gulp.src(['images/*', 'src/manifest.json'])
+        .pipe(zip('manifest.zip'))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['generate-manifest'], function() {
+    console.log('Build completed. Output in dist folder');
+});
---
+++ src/app.js
@@ -0,0 +1,37 @@
+/*jslint node: true */
+'use strict';
+
+var path = require('path');
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', function(req, res) {
+    res.render('hello');
+});
+
+app.get('/hello', function(req, res) {
+    res.render('hello');
+});
+
+app.get('/first', function(req, res) {
+    res.render('first');
+});
+
+app.get('/second', function(req, res) {
+    res.render('second');
+});
+
+app.get('/about', function(req, res) {
+    res.render('about');
+});
+
+app.get('/configure', function(req, res) {
+    res.render('configure');
+});
+
+app.listen(process.env.PORT || 3333, function() {
+    console.log('App started listening on port 3333');
+});
