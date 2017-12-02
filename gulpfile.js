7406bce38a12916c40ce2988517ed1dd11f7e31b
Charles Prakash Dasari <v-chdasa@microsoft.com>
2017-12-01 20:54:32 -0800

    Fixed an issue with image files

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ gulp.task('clean', function() {
 });
 
 gulp.task('generate-manifest', function() {
-    gulp.src(['static/images/contoso*', 'src/manifest.json'])
+    gulp.src(['src/static/images/contoso*', 'src/manifest.json'])
         .pipe(zip('helloworldapp.zip'))
         .pipe(gulp.dest('manifest'));
 });
