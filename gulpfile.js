a6cc1855f08e75ce7a71cdacde273d3ade268ad5
La Januari <v-laujan@microsoft.com>
2019-05-13 12:29:59 -0700

    Upgrade to Gulp.js 4.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,21 @@
-var gulp = require('gulp');
-var zip = require('gulp-zip');
-var del = require('del');
+const gulp = require('gulp');
+const zip = require('gulp-zip');
+const del = require('del');
 
-gulp.task('clean', function() {
+gulp.task('clean', function(done) {
     return del([
         'manifest/**/*'
-    ])
+    ], done);
 });
 
-gulp.task('generate-manifest', function() {
+gulp.task('generate-manifest', function(done) {
     gulp.src(['src/static/images/contoso*', 'src/manifest.json'])
         .pipe(zip('helloworldapp.zip'))
-        .pipe(gulp.dest('manifest'));
+        .pipe(gulp.dest('manifest'), done);
+    done();
 });
 
-gulp.task('default', ['clean', 'generate-manifest'], function() {
+gulp.task('default', gulp.series('clean', 'generate-manifest'), function(done) {
     console.log('Build completed. Output in manifest folder');
+    done();
 });
