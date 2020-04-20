4893a406daf0d5e909088600902ddeab07498367
Aditya Lele <adlele@microsoft.com>
2020-04-20 15:22:59 -0700

    simplified sending static pages

--- src/tabs.js
+++ src/tabs.js
@@ -1,46 +1,32 @@
-import fs from 'fs'
+import send from 'send'
 
 export default function tabs(server) {
 
-    var staticBasePath = 'src/views/';
     // Setup home page
     server.get('/', (req, res, next) => {
-        fs.readFile(staticBasePath + 'hello.html', 'utf8', function(err, data) {
-            if (err) throw err;
-            res.send(data);
-        });
+        send(req, 'src/views/hello.html')
+            .pipe(res)
     });
 
     // Setup the static tab
     server.get('/hello', (req, res, next) => {
-        fs.readFile(staticBasePath + 'hello.html', 'utf8', function(err, data) {
-            if (err) throw err;
-            res.send(data);
-        });
+        send(req, 'src/views/hello.html')
+            .pipe(res)
     });
 
     // Setup the configure tab, with first and second as content tabs
     server.get('/configure', (req, res, next) => {
-        fs.readFile(staticBasePath + 'configure.html', 'utf8', function(err, data) {
-            if (err) throw err;
-            res.send(data);
-        });
+        send(req, 'src/views/configure.html')
+            .pipe(res)
     });
 
     server.get('/first', (req, res, next) => {
-        fs.readFile(staticBasePath + 'first.html', 'utf8', function(err, data) {
-            if (err) throw err;
-            res.send(data);
-        });
+        send(req, 'src/views/first.html')
+            .pipe(res)
     });
 
     server.get('/second', (req, res, next) => {
-        fs.readFile(staticBasePath + 'second.html', 'utf8', function(err, data) {
-            if (err) throw err;
-            res.send(data);
-        });
+        send(req, 'src/views/second.html')
+            .pipe(res)
     });
 }
-
-
-
